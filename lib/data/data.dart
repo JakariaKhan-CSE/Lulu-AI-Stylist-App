ef7d4a2ccad6a674352985c85d424d25ce537344
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-11-17 18:45:02 +0600

    app finished.

---
+++ lib/data/data.dart
@@ -0,0 +1,18 @@
+class Actor{
+  final String profile;
+  final String link;
+  Actor( {required this.profile, required this.link});
+}
+
+List<Actor> actor = [
+Actor(profile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnzdIehqeO0jwfowzJdXfhUGTJqVoAuUCA3A&s',
+    link: 'https://www.instagram.com/alwaysramcharan?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='),
+  Actor(profile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzOBdnmLJK15QyQ5PVFKk9UlbS72qZt7pdQ&s',
+      link: 'https://www.instagram.com/actorvijay?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='),
+  Actor(profile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGjnU9DQR0y7Spxu3qWK1cuTwCcJoIkSIL8Q&s',
+      link: 'https://www.instagram.com/apnanuj?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='),
+  Actor(profile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRebDJlFS3XOqoUK6Zp-J_OD0PlE7sO2aLbHQ&s',
+      link: 'https://www.instagram.com/taniazworld?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='),
+  Actor(profile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfu9kj4TMaDErGLWQVzY1G22vjxVONr1aYkg&s',
+      link: 'https://www.instagram.com/lakshya?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==')
+];
--- lib/ui/explorer_screen.dart
+++ lib/ui/explorer_screen.dart
@@ -1,29 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../data/data.dart';
 import 'ai_agent_interaction_screen.dart';
 
 class ExplorerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(title: Text('Stylists')),
-      body: ListView.builder(
-        itemCount: 10, // Placeholder for number of stylists
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Stylist $index'),
-            subtitle: Text('Instagram Link'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => AIAgentInteractionScreen(),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
+    return Expanded(
+        child: GridView.builder(
+      itemCount: actor.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
+      itemBuilder: (context, index) {
+        Actor ac = actor[index];
+        return Column(
+          children: [
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AIAgentInteractionScreen(),
+                    ),
+                  );
+                },
+                child: Image.network(
+                  ac.profile,
+                  fit: BoxFit.cover,
+                  height: 100,
+                  width: 100,
+                )),
+            Text('Stylist ${index + 1}'),
+            ElevatedButton(
+                onPressed: () async {
+                  final Uri _url = Uri.parse(ac.link);
+                  if (!await launchUrl(_url)) {
+                    throw Exception('Could not launch $_url');
+                  }
+                },
+                child: Text('Instagram Link'))
+          ],
+        );
+      },
+    ));
+    // appBar: AppBar(title: Text('Stylists')),
+    // body: ListView.builder(
+    //   itemCount: 10, // Placeholder for number of stylists
+    //   itemBuilder: (context, index) {
+    //     return ListTile(
+    //       title: Text('Stylist $index'),
+    //       subtitle: Text('Instagram Link'),
+    //       onTap: () {
+    //         Navigator.push(
+    //           context,
+    //           MaterialPageRoute(
+    //             builder: (context) => AIAgentInteractionScreen(),
+    //           ),
+    //         );
+    //       },
+    //     );
+    //   },
+    // ),
   }
 }
